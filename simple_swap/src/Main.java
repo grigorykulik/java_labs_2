75a7667dcc6a3eaa0504e06d915baf6e7f042055
CITADEL\greg <stacheldraht23@gmail.com>
2021-03-08 23:30:50 +0300

    no message

--- simple_swap/src/Main.java
+++ simple_swap/src/Main.java
@@ -1,2 +1,47 @@
 public class Main {
+
+    public static void main(String[] args) {
+        int[] array={1, 2, 3, 4};
+        boolean s=false;
+
+        System.out.println("Initial array: ");
+        for (int i:array) {
+            System.out.print(i+" ");
+        }
+
+        s=swap(0, 3, array);
+
+        System.out.println("\nSwap successful: "+s);
+        System.out.println("Modified array: ");
+        for (int i:array) {
+            System.out.print(i+" ");
+        }
+
+        s=swap(0, 5, array);
+        System.out.println("\nSwap successful: "+s);
+        System.out.println("Modified array: ");
+        for (int i:array) {
+            System.out.print(i+" ");
+        }
+    }
+
+    public static boolean swap (int i, int j, int[] array) {
+        boolean success = false;
+        int temp = 0;
+
+        try {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+
+            success=true;
+            return success;
+        }
+
+        catch (ArrayIndexOutOfBoundsException e) {
+            return success;
+        }
+    }
+
 }
+
